f9397c5fc1a16d84b4906189f6b1a9c514a86b50
Mohamad Daher <madaher@gmail.com>
2023-07-13 22:15:15 +0300

    debugging env variable

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center ">
         <h1 className="text-2xl font-bold mb-5">Welcome to Paptales</h1>
         <h3 className="text-1xl font-bold mb-5">Stories about us</h3>
-        <p className="mb-10 max-w-md">
+        <p className="mb-10 max-w-md ">
           This is an app that generates stories about you and your loved ones.
           You can pick the story characters and settings choosing from our
           library of characters or adding your own. Paptales will generate a
